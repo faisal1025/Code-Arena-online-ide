ccd51ea2e29fc3818298bee3339c5b273ebb9af3
faisal1025 <faisalprofessional1@gmail.com>
2021-06-20 00:25:48 +0530

    port fixed

--- app.py
+++ app.py
@@ -86,5 +86,6 @@ def aboutme():
     return render_template("about_me.html")
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8080)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
         
