1f7fdb5510248af9b4ad3999fb8a390e02c36607
faisal1025 <faisalprofessional1@gmail.com>
2021-07-01 21:37:39 +0530

    working on responsiveness

--- app.py
+++ app.py
@@ -88,4 +88,5 @@ def aboutme():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    
         
---
+++ static/js/myscript.js
@@ -0,0 +1,7 @@
+// toggle button funtion navbar
+
+$(document).ready(function(){
+    $('.navbar-toggler').click(function(){
+        $('.navbar-collapse').toggleClass("collapse");
+    })
+})
